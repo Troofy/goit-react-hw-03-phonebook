5044b1a0e51d9c6249f993b846c1cba77399d0f9
Dmytro Melnyk <Melnykecho@gmail.com>
2022-06-22 11:28:27 +0300

    s2

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+root.render(<React.StrictMode>
+  <App />
+  </React.StrictMode>);
